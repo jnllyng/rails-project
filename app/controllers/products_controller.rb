06a80f37396122b5f97f3771a596a3a420c7447a
jnllyng <jueunyang00@gmail.com>
2026-03-23 01:12:40 -0500

    Add pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,16 @@
 class ProductsController < ApplicationController
-  def index
-    if params[:search].present?
-      @products = Product.joins(:category)
-                         .where("products.name LIKE ? OR categories.name LIKE ?",
-                                "%#{params[:search]}%",
-                                "%#{params[:search]}%")
-                         .includes(:category)
-    else
-      @products = Product.all.includes(:category)
-    end
+ def index
+  if params[:search].present?
+    @products = Product.joins(:category)
+                       .where("products.name LIKE ? OR categories.name LIKE ?",
+                              "%#{params[:search]}%",
+                              "%#{params[:search]}%")
+                       .includes(:category)
+                       .page(params[:page]).per(12)
+  else
+    @products = Product.all.includes(:category).page(params[:page]).per(12)
   end
+end
 
   def show
     @product = Product.find(params[:id])
