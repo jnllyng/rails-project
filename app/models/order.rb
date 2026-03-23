c8f5f93a12fa73a7bca7414e37cb93c064b6b368
jnllyng <jueunyang00@gmail.com>
2026-03-23 01:00:09 -0500

    Add validations to all models

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :address
   belongs_to :province
-  has_many :order_items
-  has_many :products, through: :order_items
+
+  validates :status, presence: true
+  validates :total, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :addresses
   has_many :orders
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
 end
